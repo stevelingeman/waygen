1cc37d8444f9b8c9e0a4728a4b15fe5d800ce2ba
Waygen <stevelingeman@gmail.com>
2025-11-21 18:49:11 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import commonjs from 'vite-plugin-commonjs'[[4](https://www.google.com/url?sa=E&q=https%3A%2F%2Fvertexaisearch.cloud.google.com%2Fgrounding-api-redirect%2FAUZIYQFnRHKA39ZMr7ku-2pEYOZrdWwZxgW6FkLzomODx-jQl4c8X1recZfGelfwWUGcP8fodOwcBZu5z6e-6qLFQN4805FlEU9IkjaSSKgSCOo4JH-6x477_wgSPFtwzjOT9JJNArP5CgfJtuSNATn1fuqy_bYEhAPIgkzr6AleRudSQxewL71XEixzGGodoPnVyw%3D%3D)]
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    commonjs() // 1.[[4](https://www.google.com/url?sa=E&q=https%3A%2F%2Fvertexaisearch.cloud.google.com%2Fgrounding-api-redirect%2FAUZIYQFnRHKA39ZMr7ku-2pEYOZrdWwZxgW6FkLzomODx-jQl4c8X1recZfGelfwWUGcP8fodOwcBZu5z6e-6qLFQN4805FlEU9IkjaSSKgSCOo4JH-6x477_wgSPFtwzjOT9JJNArP5CgfJtuSNATn1fuqy_bYEhAPIgkzr6AleRudSQxewL71XEixzGGodoPnVyw%3D%3D)] This handles the "require" conversion
+  ],
   build: {
     outDir: 'dist',
     sourcemap: false,
     commonjsOptions: {
-      // 1. Force Vite to transform CJS modules in the build
       transformMixedEsModules: true,
-      // 2. Explicitly include the problematic package
-      include: [/mapbox-gl-draw-circle/, /node_modules/],
     }
   },
   optimizeDeps: {
-    // 3. Force pre-bundling of this dependency during dev/build
     include: ['mapbox-gl-draw-circle']
   }
 })
