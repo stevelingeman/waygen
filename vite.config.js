6f7dbcccec57e088157318c51f8c9eff650a08e7
Waygen <stevelingeman@gmail.com>
2025-11-21 18:28:45 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,16 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    sourcemap: false
+    sourcemap: false,
+    commonjsOptions: {
+      // 1. Force Vite to transform CJS modules in the build
+      transformMixedEsModules: true,
+      // 2. Explicitly include the problematic package
+      include: [/mapbox-gl-draw-circle/, /node_modules/],
+    }
+  },
+  optimizeDeps: {
+    // 3. Force pre-bundling of this dependency during dev/build
+    include: ['mapbox-gl-draw-circle']
   }
 })
